37152811f10615c22b5e50242036815dc272fc70
Nishant Kadlak <nishantkadlak2015@gmail.com>
2025-01-09 22:22:37 +0530

    Create 739.Daily_temperatures.py

---
+++ 739.Daily_temperatures.py
@@ -0,0 +1,32 @@
+"""Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]"""
+
+def dailyTemperatures( temperatures: list[int]) -> list[int]:
+        st =[]
+        n = len(temperatures)
+        res = [0]*n
+
+        for i in range(n):
+            while st and temperatures[i]>temperatures[st[-1]]:
+                idx = st.pop()
+                res[idx] = i-idx
+            st.append(i)
+        
+        return res
+
+temperatures = [73,74,75,71,69,72,76,73]
+print(dailyTemperatures( temperatures))
