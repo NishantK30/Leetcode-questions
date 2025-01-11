1c23b152a6cb302b5410d337088ca7c16dfbbf1a
Nishant Kadlak <nishantkadlak2015@gmail.com>
2025-01-11 22:42:10 +0530

    Create 169.majority_element.py

---
+++ 169.majority_element.py
@@ -0,0 +1,27 @@
+'''Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2'''
+
+from collections import Counter
+def majorityElement( nums: list[int]) -> int:
+        counter = Counter(nums)
+        max_occur= 0
+        for key,value in counter.items():
+            if value > max_occur:
+                res = key
+                max_occur = value
+
+
+        if max_occur > len(nums)//2:
+            return res
